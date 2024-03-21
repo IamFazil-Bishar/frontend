f12cee8168aaf72ae8c1aaf91a94bb9f6ae1f748
IamFazil-Bishar <fazilbishar700@gamil.com>
2024-03-21 09:47:01 +0530

    t

--- src/pages/Tours.jsx
+++ src/pages/Tours.jsx
@@ -1,18 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import CommonSection from '../shared/CommonSection';
-import '../styles/tour.css';
-import TourCard from '../shared/TourCard';
-import SearchBar from '../shared/SearchBar';
-import NewsLetter from '../shared/Newsletter';
-import { Container, Row, Col } from 'reactstrap';
-import useFetch from '../hooks/useFetch';
-import { BASE_URL } from '../utils/config';
+import React, { useState, useEffect } from "react";
+import CommonSection from "../shared/CommonSection";
+import "../styles/tour.css";
+import TourCard from "../shared/TourCard";
+import SearchBar from "../shared/SearchBar";
+import NewsLetter from "../shared/Newsletter";
+import { Container, Row, Col } from "reactstrap";
+import useFetch from "../hooks/useFetch";
+import { BASE_URL } from "../utils/config";
 
 const Tours = () => {
   const [pageCount, setPageCount] = useState(0);
   const [page, setPage] = useState(0);
 
-  const { data: tours, loading, error } = useFetch(`${BASE_URL}/tours?page=${page}`);
+  const {
+    data: tours,
+    loading,
+    error,
+  } = useFetch(`${BASE_URL}/tours?page=${page}`);
   const { data: tourCount } = useFetch(`${BASE_URL}/tours/search/getTourCount`);
 
   useEffect(() => {
@@ -32,31 +36,33 @@ const Tours = () => {
         </Container>
       </section>
 
-      <section className='pt-0'>
+      <section className="pt-0">
         <Container>
-          {loading && <h4 className='text-center pt-5'>Loading....</h4>}
-          {error && <h4 className='text-center pt-5'>{error}</h4>}
+          {loading && <h4 className="text-center pt-5">Loading....</h4>}
+          {error && <h4 className="text-center pt-5">{error}</h4>}
           {!loading && !error && (
             <Row>
-              {Array.isArray(tours) && tours.map((tour) => (
-                <Col lg="3" md="6" sm="6" className='mb-4' key={tour._id}>
-                  <TourCard tour={tour} />
-                </Col>
-              ))}
+              {Array.isArray(tours) &&
+                tours.map((tour) => (
+                  <Col lg="3" md="6" sm="6" className="mb-4" key={tour._id}>
+                    <TourCard tour={tour} />
+                  </Col>
+                ))}
 
               <Col lg="12">
                 <div className="pagination d-flex align-items-center justify-content-center mt-4 gap-3">
-                  {[...Array(pageCount).keys()].map(number => (
-                    console.log("Page count:", pageCount),
-
-                    <span
-                      key={number}
-                      onClick={() => setPage(number)}
-                      className={page === number ? "active__page" : ""}
-                    >
-                      {number + 1}
-                    </span>
-                  ))}
+                  {[...Array(pageCount).keys()].map((number) => {
+                    console.log("Page count:", pageCount); // Moved outside of the return statement
+                    return (
+                      <span
+                        key={number}
+                        onClick={() => setPage(number)}
+                        className={page === number ? "active__page" : ""}
+                      >
+                        {number + 1}
+                      </span>
+                    );
+                  })}
                 </div>
               </Col>
             </Row>
