99acb466e52743ad1325ab6213ed656bf79a9a8d
IamFazil-Bishar <fazilbishar700@gamil.com>
2024-03-21 06:50:41 +0530

    featured list

--- src/components/featured-Tours/FeatruedTourList.jsx
+++ src/components/featured-Tours/FeatruedTourList.jsx
@@ -1,31 +1,30 @@
-import React from 'react'
-import TourCard from '../../shared/TourCard'
-import {Col} from 'reactstrap'
+import React from 'react';
+import TourCard from '../../shared/TourCard';
+import { Col } from 'reactstrap';
 
-import useFetch from './../../hooks/useFetch'
-import {BASE_URL} from './../../utils/config'
+import useFetch from './../../hooks/useFetch';
+import { BASE_URL } from './../../utils/config';
+
 const FeatruedTourList = () => {
+  const { data: featuredTours, loading, error } = useFetch(`${BASE_URL}/tours/search/getFeaturedTours`);
 
-  const {data: featuredTours, loading, error} = useFetch(`${BASE_URL}/tours/search/getFeaturedTours`)
+  console.log("Featured Tours:", featuredTours);
 
-  
   return (
     <>
-    {
-      loading && <h4>Loading..........</h4>
-    }
-    {
-      error && <h4>{error}</h4>
-    }
-    {!loading && !error && featuredTours?.map(tour => (
-        <Col lg = "3" md="6" sm="6" className='mb-4' key={tour._id}>
-            <TourCard tour={tour}/>
+      {
+        loading && <h4>Loading..........</h4>
+      }
+      {
+        error && <h4>{error}</h4>
+      }
+      {!loading && !error && featuredTours && featuredTours.map(tour => (
+        <Col lg="3" md="6" sm="6" className='mb-4' key={tour._id}>
+          <TourCard tour={tour}/>
         </Col>
-    ))
-
-    }
+      ))}
     </>
-  )
-}
+  );
+};
 
-export default FeatruedTourList
+export default FeatruedTourList;
