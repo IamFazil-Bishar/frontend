42e5452ce5d02fff1c3f2b6c2a932ce08582a0e2
IamFazil-Bishar <fazilbishar700@gamil.com>
2024-03-21 07:00:17 +0530

    tourList.jsx

--- src/components/featured-Tours/FeatruedTourList.jsx
+++ src/components/featured-Tours/FeatruedTourList.jsx
@@ -19,7 +19,7 @@ const FeatruedTourList = () => {
         error && <h4>{error}</h4>
       }
       {
-        !loading && !error && featuredTours?.data?.map(tour => (
+        Array.isArray(featuredTours) && featuredTours.length > 0 && featuredTours.map(tour => (
           <Col lg="3" md="6" sm="6" className='mb-4' key={tour._id}>
             <TourCard tour={tour}/>
           </Col>
