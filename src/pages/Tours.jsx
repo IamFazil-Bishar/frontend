dd0eb55af37467eae99f494a9e8b115aed07f31e
IamFazil-Bishar <fazilbishar700@gamil.com>
2024-03-21 10:02:32 +0530

    t

--- src/pages/Tours.jsx
+++ src/pages/Tours.jsx
@@ -44,7 +44,7 @@ const Tours = () => {
           {error && <h4 className="text-center pt-5">{error}</h4>}
           {!loading && !error && (
             <Row>
-              {tours?.map((tour) => (
+              {Array.isArray(tours) && tours.map((tour) => (
                 <Col lg="3" md="6" sm="6" className="mb-4" key={tour._id}>
                   <TourCard tour={tour} />
                 </Col>
