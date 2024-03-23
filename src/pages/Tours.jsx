eb9a9951947492149b6f02115493fc01d8955cb9
IamFazil-Bishar <fazilbishar700@gamil.com>
2024-03-23 17:13:40 +0530

    mm

--- src/pages/Tours.jsx
+++ src/pages/Tours.jsx
@@ -16,47 +16,30 @@ const Tours = () => {
 
   useEffect(() => {
     const pages = Math.ceil(tourCount / 8);
+    console.log('Calculated pageCount:', pages); // Log the calculated pageCount
     setPageCount(pages);
     window.scrollTo(0, 0);
   }, [page, tourCount, tours]);
 
   return (
     <>
-      <CommonSection title={'All Tours'} />
-      <section>
-        <Container>
-          <Row>
-            <SearchBar />
-          </Row>
-        </Container>
-      </section>
-
+      {/* Other JSX */}
       <section className="pt-0">
         <Container>
-          {loading && <h4 className="text-center pt-5">Loading....</h4>}
-          {error && <h4 className="text-center pt-5">{error}</h4>}
-          {!loading && !error && (
-            <Row>
-              {tours?.map((tour) => (
-                <Col lg="3" md="6" sm="6" className="mb-4" key={tour._id}>
-                  <TourCard tour={tour} />
-                </Col>
+          {/* Other JSX */}
+          <Col lg="12">
+            <div className="pagination d-flex align-items-center justify-content-center mt-4 gap-3">
+              {[...Array(pageCount).keys()].map((number) => (
+                <span
+                  key={number}
+                  onClick={() => setPage(number)}
+                  className={page === number ? 'active__page' : ''}
+                >
+                  {number + 1}
+                </span>
               ))}
-              <Col lg="12">
-                <div className="pagination d-flex align-items-center justify-content-center mt-4 gap-3">
-                  {[...Array(pageCount).keys()].map((number) => (
-                    <span
-                      key={number}
-                      onClick={() => setPage(number)}
-                      className={page === number ? 'active__page' : ''}
-                    >
-                      {number + 1}
-                    </span>
-                  ))}
-                </div>
-              </Col>
-            </Row>
-          )}
+            </div>
+          </Col>
         </Container>
       </section>
 
