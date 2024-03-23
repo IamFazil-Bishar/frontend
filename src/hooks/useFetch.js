a23f3506115d8daed6c8a698c305c304b40ebac9
IamFazil-Bishar <fazilbishar700@gamil.com>
2024-03-23 17:50:42 +0530

    mm

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const useFetch = (url) => {
+const useFetch = (url, page) => {
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
@@ -25,7 +25,7 @@ const useFetch = (url) => {
     };
 
     fetchData();
-  }, [url]);
+  }, [url, page]);
 
   return {
     data,
