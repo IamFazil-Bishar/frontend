2f9c64d3740161ceefeb8c67fcee8e2de65b604e
IamFazil-Bishar <fazilbishar700@gamil.com>
2024-03-23 17:20:07 +0530

    m

--- src/pages/Tours.jsx
+++ src/pages/Tours.jsx
@@ -23,11 +23,27 @@ const Tours = () => {
 
   return (
     <>
-      {/* Other JSX */}
+      <CommonSection title={'All Tours'} />
+      <section>
+        <Container>
+          <Row>
+            <SearchBar />
+          </Row>
+        </Container>
+      </section>
+
       <section className="pt-0">
         <Container>
-          {/* Other JSX */}
-          <Col lg="12">
+          {loading && <h4 className="text-center pt-5">Loading....</h4>}
+          {error && <h4 className="text-center pt-5">{error}</h4>}
+          {!loading && !error && (
+            <Row>
+              {tours?.map((tour) => (
+                <Col lg="3" md="6" sm="6" className="mb-4" key={tour._id}>
+                  <TourCard tour={tour} />
+                </Col>
+              ))}
+                <Col lg="12">
             <div className="pagination d-flex align-items-center justify-content-center mt-4 gap-3">
               {[...Array(pageCount).keys()].map((number) => (
                 <span
@@ -40,6 +56,8 @@ const Tours = () => {
               ))}
             </div>
           </Col>
+            </Row>
+          )}
         </Container>
       </section>
 
