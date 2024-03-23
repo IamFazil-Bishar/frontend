bfedf10a30fa36ae3c80150f2946cedf242f5367
IamFazil-Bishar <fazilbishar700@gamil.com>
2024-03-23 17:24:59 +0530

    mm

--- src/pages/Tours.jsx
+++ src/pages/Tours.jsx
@@ -36,28 +36,28 @@ const Tours = () => {
         <Container>
           {loading && <h4 className="text-center pt-5">Loading....</h4>}
           {error && <h4 className="text-center pt-5">{error}</h4>}
-          {!loading && !error && (
-            <Row>
-              {tours?.map((tour) => (
-                <Col lg="3" md="6" sm="6" className="mb-4" key={tour._id}>
-                  <TourCard tour={tour} />
-                </Col>
-              ))}
-                <Col lg="12">
-            <div className="pagination d-flex align-items-center justify-content-center mt-4 gap-3">
-              {[...Array(pageCount).keys()].map((number) => (
-                <span
-                  key={number}
-                  onClick={() => setPage(number)}
-                  className={page === number ? 'active__page' : ''}
-                >
-                  {number + 1}
-                </span>
-              ))}
-            </div>
-          </Col>
-            </Row>
-          )}
+          {!loading && !error && Array.isArray(tours) && (
+  <Row>
+    {tours.map((tour) => (
+      <Col lg="3" md="6" sm="6" className="mb-4" key={tour._id}>
+        <TourCard tour={tour} />
+      </Col>
+    ))}
+    <Col lg="12">
+      <div className="pagination d-flex align-items-center justify-content-center mt-4 gap-3">
+        {[...Array(pageCount).keys()].map((number) => (
+          <span
+            key={number}
+            onClick={() => setPage(number)}
+            className={page === number ? "active__page" : ""}
+          >
+            {number + 1}
+          </span>
+        ))}
+      </div>
+    </Col>
+  </Row>
+)}
         </Container>
       </section>
 
