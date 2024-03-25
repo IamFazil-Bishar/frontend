32b69dc97f419cbb6bf05e5d1a97f63b1b879638
IamFazil-Bishar <fazilbishar700@gamil.com>
2024-03-25 15:23:03 +0530

    credintial

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,37 +1,47 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 const useFetch = (url) => {
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+  const { user } = useContext(AuthContext);
 
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
 
       try {
-        const res = await fetch(url);
+        if (!user || !user.token) {
+          // Handle the case where the user or user.token is null
+          throw new Error("User not authenticated");
+        }
+
+        const res = await fetch(url, {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+          credentials: 'include', // Include credentials such as cookies
+        });
 
         if (!res.ok) {
-          setError("failed to fetch");
+          setError("Failed to fetch data");
+          return;
         }
+
         const result = await res.json();
         setData(result.data);
-        setLoading(false)
-      } catch (err) {
-        setError(err.message);
+      } catch (error) {
+        setError(error.message);
+      } finally {
         setLoading(false);
       }
     };
 
     fetchData();
-  }, [url]);
+  }, [url, user]);
 
-  return {
-    data,
-    error,
-    loading,
-  };
+  return { data, loading, error };
 };
 
 export default useFetch;
